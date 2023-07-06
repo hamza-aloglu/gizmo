fd1f70da924eeb2d7025e2fd1d7bc0bbbdf8da44
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-07-06 21:20:26 +0300

    create resource server

---
+++ resourceserver/src/main/java/com/example/resourceserver/ResourceserverApplication.java
@@ -0,0 +1,47 @@
+package com.example.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableMethodSecurity
+public class ResourceserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceserverApplication.class, args);
+    }
+
+}
+
+@Service
+class HomeService {
+
+//    @AuthenticationPrincipal Jwt jwt
+    @PreAuthorize("hasAnyAuthority('SCOPE_openid')")
+    public Jwt getPrincipal() {
+        return (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
+
+
+@RestController
+class HomeController {
+    private HomeService service;
+
+    HomeController(HomeService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String testHome() {
+        return  "Hello, World resource server! " + service.getPrincipal().getSubject();
+    }
+}
---
+++ resourceserver/src/main/java/com/example/resourceserver/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.resourceserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .cors(Customizer.withDefaults())
+                .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("http://127.0.0.1:3000");
+        config.addAllowedOrigin("http://127.0.0.1:4200");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
