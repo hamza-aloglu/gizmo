f4b58102e82ae2bca495d0066d1a5853ab7efa99
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-07-08 14:45:59 +0300

    return data in JSON format

--- authserver/src/main/java/com/example/authserver/exception/UserAlreadyExistsAuhenticationExceptionHandler.java
+++ authserver/src/main/java/com/example/authserver/exception/UserAlreadyExistsAuhenticationExceptionHandler.java
@@ -5,12 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
 @ControllerAdvice
 public class UserAlreadyExistsAuhenticationExceptionHandler {
     @ExceptionHandler(value = UserAlreadyExistsAuthenticationException.class)
-    public ResponseEntity<UserAlreadyExistsAuthenticationException> handleUserExistsException(
+    public ResponseEntity<Map<String, String>> handleUserExistsException(
             UserAlreadyExistsAuthenticationException ex) {
         System.out.println(ex.getMessage());
-        return new ResponseEntity<>(ex, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(Collections.singletonMap("ERROR:", ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
