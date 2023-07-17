f53410c15a989189e0265f68ac0ea58bcfd2e20e
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-07-17 13:16:32 +0300

    convert list of cards to list of cardDtos

--- resourceserver/src/main/java/com/example/resourceserver/mapper/CardMapper.java
+++ resourceserver/src/main/java/com/example/resourceserver/mapper/CardMapper.java
@@ -5,8 +5,11 @@ import com.example.resourceserver.dto.CardDto;
 import com.example.resourceserver.model.Card;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CardMapper {
     CardDto cardToCardDto(Card card);
     Card cardCreateRequestToCard(CardCreateRequest cardCreateRequest);
+    List<CardDto> listCardToListCardDto(List<Card> cards);
 }
