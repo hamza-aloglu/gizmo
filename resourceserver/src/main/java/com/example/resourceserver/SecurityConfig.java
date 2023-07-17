ebca2bce95b00e7f26c1b3a7e07b44f4ae1f2e73
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-07-17 10:20:17 +0300

    config h2 and JPA dependencies

--- resourceserver/src/main/java/com/example/resourceserver/SecurityConfig.java
+++ resourceserver/src/main/java/com/example/resourceserver/SecurityConfig.java
@@ -4,17 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // h2 console configurations
+        http
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(toH2Console()).permitAll()
+                );
+
         return http
                 .authorizeHttpRequests(auth -> {
                     auth.anyRequest().authenticated();
