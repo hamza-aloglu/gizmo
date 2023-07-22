183e466b2d06ea9eae19bbd2bbf97cfa58453525
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-07-22 19:44:19 +0300

    create basic css column structure

--- react-client/src/components/kanban/Board.js
+++ react-client/src/components/kanban/Board.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Column from "./Column";
 import KanbanService from "../../services/KanbanService";
+import "../../css/kanban/Board.css";
 
 const Board = ({ title, kanbanColumnsResponse, boardId }) => {
     const [kanbanColumns, setKanbanColumns] = useState(kanbanColumnsResponse);
@@ -11,11 +12,13 @@ const Board = ({ title, kanbanColumnsResponse, boardId }) => {
         e.preventDefault();
         KanbanService.createColumn(newColumnTitle, boardId).then(async (response) => {
             const column = await response.json();
-            if(response.ok) {
-                const newKanbanColumns = [column];
-                if(kanbanColumns != null) {
+            if (response.ok) {
+                const newKanbanColumns = [];
+                if (kanbanColumns != null) {
                     newKanbanColumns.push(...kanbanColumns);
                 }
+                newKanbanColumns.push(column);
+
                 setKanbanColumns(newKanbanColumns);
             }
         });
@@ -24,19 +27,25 @@ const Board = ({ title, kanbanColumnsResponse, boardId }) => {
 
     return (
         <div>
-            <h2 style={{textAlign: "center", margin: "10px"}}> {title} </h2>
-            {kanbanColumns && kanbanColumns.map(kanbanColumn => (
-                <div key={kanbanColumn.title} style={{
-                    display: "inline-block",
-                    width: "20%"
-                }}>
-                    <Column title={kanbanColumn.title} cardsResponse={kanbanColumn.cards} columnId={kanbanColumn.id} />
+            <h2 id="board-title"> {title} </h2>
+            <div id="columns-wrapper">
+                {kanbanColumns && kanbanColumns.map(kanbanColumn => (
+                    <div className="column-container">
+                        <div key={kanbanColumn.title} className="column-wrapper">
+                            <Column title={kanbanColumn.title} cardsResponse={kanbanColumn.cards} columnId={kanbanColumn.id} />
+                            hi
+                        </div>
+                    </div>
+                ))}
+                <div className="column-container">
+                    <div className="column-wrapper">
+                        {!isFormActive && <button onClick={() => setIsFormActive(true)}> Create Column </button>}
+                        {isFormActive && <form style={{ display: "inline-block" }} onSubmit={handleCreateColumn}>
+                            <input type="text" onChange={(e) => setNewColumnTitle(e.target.value)} />
+                        </form>}
+                    </div>
                 </div>
-            ))}
-            {!isFormActive && <button onClick={() => setIsFormActive(true)}> Create Column </button>}
-            {isFormActive && <form style={{display: "inline-block"}} onSubmit={handleCreateColumn}>
-                <input type="text" onChange={(e) => setNewColumnTitle(e.target.value)} />
-            </form> }
+            </div>
         </div>
     )
 }
