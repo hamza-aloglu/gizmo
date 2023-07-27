597b34b0e601a1d1c865ddee87935fd274445aa1
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-07-27 19:58:05 +0300

    create test page for newly added features

--- react-client/src/components/Welcome.js
+++ react-client/src/components/Welcome.js
@@ -1,9 +1,37 @@
-import jwt_decode from 'jwt-decode';
+import { useEffect, useState } from "react";
+import KanbanService from "../services/KanbanService";
+import Board from "./kanban/Board";
+import { useNavigate } from "react-router";
 
 const Welcome = () => {
+    const [boards, setBoards] = useState([]);
+    const [title, setTitle] = useState([]);
+    const [isFormActive, setIsFormActive] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        KanbanService.fetchBoards().then(async (response) => {
+            const boards = await response.json();
+            setBoards(boards);
+        });
+    }, [])
+
+    function handleCreateBoard(e) {
+        e.preventDefault();
+        KanbanService.createBoard(title).then(async (response) => {
+            const board = await response.json();
+            navigate('/board/' + board.id, { state: board });
+        });
+    }
+
     return (
         <div>
             welcomepage
+            {!isFormActive && <button onClick={() => setIsFormActive(true)}> Create Board </button>}
+            {isFormActive && <form onSubmit={handleCreateBoard}>
+                <input type="text" onChange={(e) => setTitle(e.target.value)} />
+            </form> }
+            
         </div>
     )
 }
