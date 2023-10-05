a1313315d58c6f133c61f9b40d633fd36baf382c
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-10-05 11:31:46 +0300

    Run tests using in memory database while building image

--- resourceserver/src/test/java/com/example/resourceserver/ResourceserverApplicationTests.java
+++ resourceserver/src/test/java/com/example/resourceserver/ResourceserverApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.resourceserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+        "spring.profiles.active=test"
+})
 class ResourceserverApplicationTests {
 
     @Test
