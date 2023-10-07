1871d4535fa382672f923ffc0b35ebfda2fe4ba9
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-10-07 12:56:28 +0300

    fix redirection of client

--- authserver/src/main/java/com/example/authserver/controller/LoginController.java
+++ authserver/src/main/java/com/example/authserver/controller/LoginController.java
@@ -1,13 +1,20 @@
 package com.example.authserver.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+    @Value("${CLIENT_URI:http://127.0.0.1:3000}")
+    private String clientUri;
+
     @GetMapping("/login")
-    String getLogin() {
-        return "login";
+    public ModelAndView getLogin(Model model) {
+        ModelAndView mav = new ModelAndView("login");
+        mav.addObject("clientRegisterUri", clientUri + "/register");
+        return mav;
     }
 }
