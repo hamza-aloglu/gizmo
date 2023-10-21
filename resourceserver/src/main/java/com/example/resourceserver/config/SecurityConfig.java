40500ff788fda140b5d597fc1243a8d8e7d0a5bd
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-10-21 12:47:48 +0300

    create config package

--- resourceserver/src/main/java/com/example/resourceserver/config/SecurityConfig.java
+++ resourceserver/src/main/java/com/example/resourceserver/config/SecurityConfig.java
@@ -1,11 +1,10 @@
-package com.example.resourceserver;
+package com.example.resourceserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
