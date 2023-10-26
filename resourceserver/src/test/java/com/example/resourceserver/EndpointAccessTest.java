2f9eee5f7b6fca451b920d27d04c5b586f53382f
Hamza Aloglu <hamza-aloglu@hotmail.com>
2023-10-26 19:30:55 +0300

    test: actuator endpoint can only be accessed by admin

---
+++ resourceserver/src/test/java/com/example/resourceserver/EndpointAccessTest.java
@@ -0,0 +1,33 @@
+package com.example.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EndpointAccessTest {
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @WithMockUser(roles = "ADMIN")
+    @Test
+    public void givenAdminRole_whenAccessActuator_thenOk() throws Exception {
+        mockMvc.perform(get("/actuator"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void givenUserRole_whenAccessActuator_thenForbidden() throws Exception {
+        mockMvc.perform(get("/actuator"))
+                .andExpect(status().isForbidden());
+    }
+
+}
